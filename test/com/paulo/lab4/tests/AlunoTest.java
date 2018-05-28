42510e1a97406d004760ff81d838bfaad3da39a0
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-05-28 19:28:29 -0300

    95%

--- test/com/paulo/lab4/tests/AlunoTest.java
+++ test/com/paulo/lab4/tests/AlunoTest.java
@@ -1,24 +1,38 @@
 package com.paulo.lab4.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import com.paulo.lab4.Aluno;
+
 class AlunoTest {
-
+	
+	private Aluno a1;
+	private Aluno a2;
+	private Aluno a3;
+	
+	@Before
+	public void criaAluno() {
+		a1 = new Aluno("120", "Pedro Viana", "Ciência da computação");
+		a2 = new Aluno("110", "Paulo Júnior", "Engenharia Elétrica");
+		a3 = new Aluno("130", "Ana Vilela", "Administração");
+	}
+	
 	@Test
-	void testAluno() {
-		fail("Not yet implemented");
+	public void testAluno() {
+		Aluno a = new Aluno("117210122", "Pedro Júlio", "Engenharia de materiais");
 	}
 
 	@Test
-	void testToString() {
-		fail("Not yet implemented");
+	public void testToString() {
+		assertEquals("110 - Paulo Júnior - Engenharia Elétricas", a2.toString());
 	}
 
 	@Test
-	void testEqualsObject() {
-		fail("Not yet implemented");
+	public void testEqualsObject() {
+		assertEquals(a1, a2);
 	}
 
 }
