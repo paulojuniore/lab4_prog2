1520305ae1ae22354f197b0ea3d40871a11e8599
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-05-31 18:46:35 -0300

    Finished

---
+++ test/com/paulo/lab4/tests/ControleTest.java
@@ -0,0 +1,44 @@
+package com.paulo.lab4.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControleTest {
+
+	@Test
+	void testCadastraAluno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExibirAluno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCadastrarGrupo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAlocarAlunoEmGrupo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testImprimirAlunosDeGrupo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegistrarAlunoQueRespondeu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testImprimirRegistroDeAlunos() {
+		fail("Not yet implemented");
+	}
+
+}
